1959c37dcb4cf2e694cc6db1eb6eceabc1fcf472
Пахаев Хусейн <pakhaev01@gmail.com>
2023-04-12 14:05:59 +0300

    Delete extensions file and enum

--- ColorChagneApp/Extensions + ColorChangeVC.swift
+++ ColorChagneApp/Extensions + ColorChangeVC.swift
@@ -7,21 +7,4 @@
 
 import UIKit
 
-extension ColorChangeVC {
-    func setupColorView() {
-        colorView.backgroundColor = UIColor(
-            red: CGFloat(redSlider.value),
-            green: CGFloat(greenSlider.value),
-            blue: CGFloat(blueSlider.value),
-            alpha: 1
-        )
-    }
-}
 
-extension ColorChangeVC {
-    enum ColorRGB: String {
-        case red
-        case green
-        case blue
-    }
-}
